6c15a5dbaf9e33b0ab7064afca1e829eb2033608
rifatNR <Rifatnoor92@gmail.com>
2022-12-01 06:27:59 +0600

    unknown changes

--- Assets/Brackyes Third Person Ctrl/PlayerCtrl.cs
+++ Assets/Brackyes Third Person Ctrl/PlayerCtrl.cs
@@ -11,20 +11,32 @@ public class PlayerCtrl : MonoBehaviour
     public float turnSpeed = 0.1f;
     float currentVelocity;
 
+    float H;
+    float V;
+
+    Animator anim;
+
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
         camera = Camera.main;
+        anim = GetComponent<Animator>();
     }
 
 
     void Update()
     {
-        float H = Input.GetAxisRaw("Horizontal");
-        float V = Input.GetAxisRaw("Vertical");
+        Movement();
+        UpdateAnimation();
+    }
+
+    private void Movement()
+    {
+        H = Input.GetAxisRaw("Horizontal");
+        V = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(H, 0, V).normalized;
 
-        if(direction.magnitude >= 0.1f)
+        if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.transform.eulerAngles.y;
             float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref currentVelocity, turnSpeed);
@@ -33,5 +45,11 @@ public class PlayerCtrl : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
             characterController.Move(moveDir.normalized * moveSpeed * Time.deltaTime);
         }
+    }
+
+    private void UpdateAnimation()
+    {
+        float animMove = Mathf.Clamp01(Mathf.Abs(H) + Mathf.Abs(V));
+        anim.SetFloat("Vertical", animMove);
     }
 }
